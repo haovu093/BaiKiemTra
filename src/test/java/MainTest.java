ef6276b043d6ed55aa0fd6a09f3c22cb1bddfb30
HaoVT <haovt7@onemount.com>
2021-06-15 10:07:51 +0700

    first commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,10 +6,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MainTest {
     @Test
     void testB1(){
-        Main main = new Main();
+        Bai1 b1 = new Bai1();
         String str = "You Only Live Once. But if You do it right. once is Enough";
-        assertThat(str).isNotNull()
-                .isEqualTo(7);
+        assertThat(b1.demTu(str)).isEqualTo(7)
+                .isNotNull();
+        assertThat(b1.chuyentuInhoa(str)).isEqualTo("You Only Live Once. But If You Do It Right. Once Is Enough")
+                .isNotNull();
     }
 
 }
